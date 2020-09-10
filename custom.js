bf48405fea0886cef3feefa02b0cdf6d342de99b
Ben Parker <bparker@gmail.com>
2020-09-10 12:45:42 -0500

    Added in comments

--- custom.js
+++ custom.js
@@ -1,11 +1,14 @@
+// set credit
 $("#credit").click(function () {
   localStorage.setItem("method", "credit");
 });
 
+// set wire
 $("#wire").click(function () {
   localStorage.setItem("method", "wire");
 });
 
+// if else
 $(document).ready(function () {
   if (localStorage.getItem("method") == "credit") {
     console.log("credit");
