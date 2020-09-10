c1eb5e1428d682503685d83366413d9bfdbc794c
Ben Parker <bparker@gmail.com>
2020-09-10 13:16:24 -0500

    Added hide show functionality

--- custom.js
+++ custom.js
@@ -8,11 +8,25 @@ $("#wire").click(function () {
   localStorage.setItem("method", "wire");
 });
 
+$("#wireswitch").click(function () {
+  localStorage.setItem("method", "wire");
+  $(".credit-block").hide();
+  $(".wire-block").show();
+});
+
+$("#creditswitch").click(function () {
+  localStorage.setItem("method", "credit");
+  $(".wire-block").hide();
+  $(".credit-block").show();
+});
+
 // if else
 $(document).ready(function () {
   if (localStorage.getItem("method") == "credit") {
+    $(".credit-block").show();
     console.log("credit");
   } else if (localStorage.getItem("method") == "wire") {
+    $(".wire-block").show();
     console.log("wire");
   } else {
     console.log("nothing");
